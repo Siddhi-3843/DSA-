0926e6350537d50b0a692341cbba4f02e7710644
Siddhi <siddhideshmukh141099@gmail.com>
2026-05-13 19:01:21 +0530

    Day 1 complete: second largest and list reversal

--- day_01.py
+++ day_01.py
@@ -1,3 +1,17 @@
+# swapping using two pointers 
+a=[]
+left = 0
+right =len(a)-1
+
+while left<right:
+    a[left],a[right]=a[right],a[left]
+    left +=1
+    right -=1
+print(a)
+
+
+
+
 #Find Largest and second largest number of array 
 a=[5, 4, 3, 2, 1]
 largest= a[0]
