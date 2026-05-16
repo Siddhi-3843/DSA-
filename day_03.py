1529b45cf14f3c5129a10d12108e53c69d095534
Siddhi <siddhideshmukh141099@gmail.com>
2026-05-16 14:13:56 +0530

    Day 3: Third largest, Reverse array, Rotate array - all 3 approaches

--- day_03.py
+++ day_03.py
@@ -1,3 +1,18 @@
+# rotate an array by k times optimal solution
+
+
+# rotate an array by k times
+arr = [1, 2, 3, 4, 5]
+k = 2
+n = len(arr)
+for i in range(k):
+    last = arr[n-1]
+    for i in range(n-1, 0, -1):
+        arr[i] = arr[i-1]
+    arr[0] = last 
+print(arr)
+
+
 # third largest element 
 arr = [5, 5, 5]
 largest =float('-inf')
