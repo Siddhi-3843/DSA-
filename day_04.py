4d4aff5dc8d9742ad0fb308836a9a013e4e924aa
Siddhi Deshmukh <siddhideshmukh141099@gmail.com>
2026-05-17 21:38:23 +0530

    Day 4: Wave Array - O(n) solution

--- day_04.py
+++ day_04.py
@@ -1,3 +1,19 @@
+# wave array
+arr = [3, 6, 5, 10, 7, 20]
+n = len(arr)
+
+for i in range(0, n, 2):    # even indexes only
+    
+    if i > 0 and arr[i] < arr[i-1]:     
+        arr[i], arr[i-1] = arr[i-1], arr[i]
+    
+    if i < n-1 and arr[i] < arr[i+1]:    
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+
+print(arr)
+
+
+
 # move all zeros to end
 arr = [0, 1, 0, 3, 12]
 j = 0
