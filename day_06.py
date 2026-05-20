6dc29628c9f4e21461da761cd8e618e3eeaace42
Siddhi Deshmukh <siddhideshmukh141099@gmail.com>
2026-05-21 00:46:05 +0530

    Day 5: Reverse Array in Groups

--- day_06.py
+++ day_06.py
@@ -1,3 +1,23 @@
+# Reverse array in group
+arr = [1, 2, 3, 4, 5]
+k = 3
+n = len(arr)
+
+i = 0
+while i < n:
+    left = i
+    right = min(i + k - 1, n - 1)  # handles last group
+    
+    while left < right:
+        arr[left], arr[right] = arr[right],arr[left]
+        left += 1
+        right -= 1
+    
+    i +=k   # jump to next group
+
+print(arr)
+
+
 # Remove duplicates from sorted array
 arr = [1, 1, 1, 1, 2]
 j = 0
