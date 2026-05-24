5e6c29bfd6cc1a6f7eb65f8c958978933710d160
Siddhi Deshmukh <siddhideshmukh141099@gmail.com>
2026-05-24 20:09:48 +0530

    Day 5: Array Leaders

--- day_05.py
+++ day_05.py
@@ -1,3 +1,19 @@
+#Array Leaders in an array
+
+arr = [16, 17, 4, 3, 5, 2]
+n = len(arr)
+leaders = []
+max_right = arr[n-1]
+leaders.append(max_right)
+
+for i in range(n-2, -1, -1):
+    if arr[i] > max_right:
+        leaders.append(arr[i])
+        max_right = arr[i]
+
+leaders.reverse()
+print(leaders)
+
 # Buy and sell stock (multiple transactions allowed)    prices = [7, 1, 5, 3, 6, 4]
 prices = [7, 1, 5, 3, 6, 4]
 profit = 0
